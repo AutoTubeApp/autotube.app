8ec06cadd3e4187ab80a1b2339b8475b97e13537
Stéphane Depierrepont <toorop@gmail.com>
2021-06-26 11:58:57 +0200

    dl boxes

--- components/Download.vue
+++ components/Download.vue
@@ -1,74 +1,87 @@
 <template>
-  <v-row class="d-flex">
-    <v-row
-      v-if="$fetchState.pending"
-      class="d-flex"
-    >
-      <v-col
-        id="loading"
-        cols="12"
-        sm="12"
-        class="d-flex justify-center align-center mt-10"
+  <v-container>
+    <v-row class="d-flex">
+      <v-row
+        v-if="$fetchState.pending"
+        class="d-flex"
       >
-        <v-progress-circular
-          :size="80"
-          :width="7"
-          color="primary"
-          indeterminate
-        />
-      </v-col>
-    </v-row>
-    <v-row v-if="!$fetchState.pending" class="d-flex mt-5">
-      <v-col
-        v-for="card in cards"
-        :key="card.title"
-        cols="12"
-        sm="4"
-      >
-        <v-card
-          :class="[card.selected ? 'cardSelected' : 'card']"
-          outlined
+        <v-col
+          id="loading"
+          cols="12"
+          sm="12"
+          class="d-flex justify-center align-center mt-10"
         >
-          <v-row class="d-flex ma-1">
-            <v-col
-              cols="12"
-              xs="12"
-              sm="4"
-              class="d-flex  pa-0"
-            >
-              <v-img
-                :src="card.src"
-                max-height="100px"
-                max-width="100px"
-              />
-            </v-col>
-            <v-col
-              cols="12"
-              xs="12"
-              sm="8"
-              class="d-flex justify-center align-center pa-0"
-            >
-              <p class="mb-2">
-                {{ card.filename }}
-              </p>
-            </v-col>
-          </v-row>
-          <v-card-actions
-            class="pt-0"
+          <v-progress-circular
+            :size="80"
+            :width="7"
+            color="primary"
+            indeterminate
+          />
+        </v-col>
+      </v-row>
+      <v-row v-if="!$fetchState.pending" class="d-flex mt-5 justify-center">
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          cols="12"
+          xl="3"
+          lg="3"
+          md="4"
+          sm="12"
+          xs="12"
+        >
+          <v-card
+            :class="[card.selected ? 'cardSelected' : 'card']"
+            outlined
           >
-            <v-spacer />
-            <v-btn
-              class="white--text"
-              color="primary"
-              :href="card.link"
-            >
-              Download
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
+            <v-row class="d-flex ma-1">
+              <v-col
+                cols="4"
+                lg="3"
+                md="4"
+                sm="2"
+                xs="4"
+
+                class="d-flex pa-0"
+              >
+                <v-img
+                  :src="card.src"
+                  max-height="100px"
+                  max-width="100px"
+                />
+              </v-col>
+
+              <v-col
+                cols="8"
+                lg="9"
+                md="8"
+                sm="10"
+                xs="8"
+                class="d-flex flex-row justify-end align-center pa-0 pt-2 pt-xl-0"
+              >
+                <!--
+                <span
+                  class="text-left text-xl-center pt-xl-0"
+                >
+                  {{ card.filename }}</span>
+                  -->
+                <!--<v-spacer /> -->
+                <span class="d-flex justify-end ">
+                  <v-btn
+                    class="white--text mb-3 mr-2 mt-xl-3 mr-xl-4 text-right"
+                    color="primary"
+                    :href="card.link"
+                  >
+                    Download
+                  </v-btn>
+                </span>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-row>
-  </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -84,19 +97,19 @@ export default {
       cards: {
         mac: {
           src: '/img/apple.svg',
-          filename: 'autotube-0.0.3.dmg',
+          filename: 'autotube.dmg',
           link: '',
           selected: true
         },
         windows: {
           src: '/img/windows.svg',
-          filename: 'autotube Setup 0.0.48.exe',
+          filename: 'autotube-setup.exe',
           link: '',
           selected: false
         },
         linux: {
           src: '/img/linux.svg',
-          filename: 'autotube-0.0.40.AppImage',
+          filename: 'autotube.AppImage',
           link: '',
           selected: false
         }
@@ -120,7 +133,7 @@ export default {
     async getLatest () {
       const latest = await this.$axios.$get('/api/latest-packages')
       oss.forEach((os) => {
-        this.cards[os].filename = latest[os]
+        // this.cards[os].filename = latest[os]
         this.cards[os].link = `${repoBaseURL}${latest[os]}`
       })
     }
